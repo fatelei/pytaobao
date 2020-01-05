eee052d9575350fcfd4d347a523fb7cc3f60c3d9
fatelei <fatelei@gmail.com>
2020-01-05 11:16:06 +0800

    fix: fix release

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 # -*- coding: utf8 -*-
 
 from setuptools import find_packages, setup
+from pytaobao import __version__
 
 setup(
     name='pytaobao',
-    version='1.0.0',
+    version=__version__,
     license='PRIVATE',
     author='fatelei',
     author_email='fatelei@gmail.com',
     url='https://github.com/fatelei/pytaobao',
     description='voom backend',
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(where='.', exclude=['tests', 'scriptss']),
     zip_safe=False,
 )
